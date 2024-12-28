000cab7f883b8b335499dd13772dfe54a17e9d11
grassblock233 <w15067120025@outlook.com>
2024-12-29 00:03:43 +0800

    修改配置文件

--- src/consts.ts
+++ src/consts.ts
@@ -2,40 +2,53 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_LANG = "en";
-export const SITE_TAB = "Frosti";
-export const SITE_TITLE = "Frosti 🧊";
-export const SITE_DESCRIPTION = "A blog template";
+export const SITE_LANG = "zh";
+export const SITE_TAB = "GrassBlock";
+export const SITE_TITLE = "GrassBlock";
+export const SITE_DESCRIPTION = "GrassBlock233's Blog";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
-export const USER_SITE = "https://frosti.saroprock.com"
+export const USER_NAME = "GrassBlock233";
+export const USER_SITE = "https://grassblock233.github.io/";
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
-export const SERVER_URL = "https://demo.saroprock.com";
+export const SERVER_URL = "https://grassblock233.github.io/";
 
 // Theme settings
 export const DAISYUI_THEME = {
   light: "winter",
   dark: "dracula",
-}
+};
 export const CODE_THEME = {
   light: "github-light",
   dark: "github-dark",
-}
+};
 
 // Menu items for navigation
 export const menuItems = [
-  { id: "home", text: "Home", href: "/", svg: "material-symbols:home-outline-rounded", target: "_self" }, // Home page
-  { id: "about", text: "About", href: "/about", svg: "material-symbols:info-outline-rounded", target: "_self" }, // About page
+  {
+    id: "home",
+    text: "主页",
+    href: "/",
+    svg: "material-symbols:home-outline-rounded",
+    target: "_self",
+  }, // Home page
+  {
+    id: "about",
+    text: "关于",
+    href: "/about",
+    svg: "material-symbols:info-outline-rounded",
+    target: "_self",
+  }, // About page
   {
     id: "blog",
-    text: "Blogs",
+    text: "文章",
     href: "/blog",
     svg: "material-symbols:book-2-outline-rounded",
     target: "_self",
+    /*
     subItems: [
       {
         id: "all",
@@ -59,38 +72,26 @@ export const menuItems = [
         target: "_self",
       }, // Life category
     ],
+    */
   }, // Blog page with sub-items
   {
     id: "project",
-    text: "Project",
+    text: "项目",
     href: "/project",
     svg: "material-symbols:code-blocks-outline",
     target: "_self",
   }, // Projects page
   {
     id: "friend",
-    text: "Friend",
+    text: "友链",
     href: "/friend",
     svg: "material-symbols:supervisor-account-outline-rounded",
     target: "_self",
   }, // Friends page
-  {
-    id: "contact",
-    text: "Contact",
-    href: "mailto:contact.evesunmaple@outlook.com", // Contact email
-    target: "_blank", // Open in a new tab
-    svg: "material-symbols:attach-email-outline-rounded",
-  },
 ];
 
 // Social media and contact icons
 export const socialIcons = [
-  {
-    href: "https://afdian.net/a/saroprock",
-    ariaLabel: "Support my work",
-    title: "Support my work",
-    svg: "ri:cup-line",
-  },
   {
     href: "https://github.com/EveSunMaple",
     ariaLabel: "Github",
